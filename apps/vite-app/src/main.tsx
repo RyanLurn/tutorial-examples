48d0d531f69c7432a19fdca71d81405d2a5effe2
Ryan Luong <lumintriet@gmail.com>
2025-10-02 09:53:04 +0700

    Mode toggle component.

--- apps/vite-app/src/main.tsx
+++ apps/vite-app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@repo/ui/globals.css";
+import { ModeToggle } from "@repo/ui/components/mode-toggle";
 import { ThemeProvider } from "@repo/ui/components/theme-provider";
 import App from "@/App";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <ModeToggle variant="outline" className="fixed top-3 right-3" />
     </ThemeProvider>
   </StrictMode>
 );
